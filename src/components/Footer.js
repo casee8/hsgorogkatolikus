adc99cddc5bfe85969cadb63af97b4ba1e2fc3fa
casee8 <pokerkaszy7@gmail.com>
2021-04-20 16:28:07 +0200

    Add links to different webpages with icons + style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,76 @@
 import React from "react";
-import ICXC_NIKA from "../media/logo-szukre-mediakozpont2019-436x429-1.png";
+import LazyLoad from "react-lazy-load";
+import gorogkatolikusMetropolia from "../media/iconsForLinks/gorogkatolikusmetropolia.jpg";
+import hajdudorogiFoegyhazmegye from "../media/iconsForLinks/hajdudorogi-foegyhazmegye.jpg";
+import nyiregyhaziEgyhazmegye from "../media/iconsForLinks/nyiregyhazi-foegyhazmegye.jpg";
+import miskolciEgyhazmegye from "../media/iconsForLinks/miskolci-egyhazmegye.jpg";
+import mariapocsNemzetiKegyhely from "../media/iconsForLinks/mariapocs-nemzeti-kegyhely.jpg";
+import szentLukacsGkatSzeretetszolgalat from "../media/iconsForLinks/szent-lukacs-gorogkatolikus-szeretetszolgalat.jpg";
 
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
+      <div className="flex icon-links-container">
+        <LazyLoad className="all-center" debounce={false} offsetVertical={500}>
+          <a href="https://www.gorogkatolikus.hu" target="_blank" rel='noreferrer noopener'>
+            <img
+              className="icon-links"
+              src={gorogkatolikusMetropolia}
+              alt="Görögkatolikus Metropólia"
+            />
+          </a>
+        </LazyLoad>
+        <LazyLoad className="all-center" debounce={false} offsetVertical={500}>
+          <a href="https://www.hd.gorogkatolikus.hu" target="_blank" rel='noreferrer noopener'>
+            <img
+              className="icon-links"
+              src={hajdudorogiFoegyhazmegye}
+              alt="Hajdúdorogi Főegyházmegye"
+            />
+          </a>
+        </LazyLoad>
+        <LazyLoad className="all-center" debounce={false} offsetVertical={500}>
+          <a href="https://www.nyirgorkat.hu" target="_blank" rel='noreferrer noopener'>
+            <img
+              className="icon-links"
+              src={nyiregyhaziEgyhazmegye}
+              alt="Nyíregyházi Egyházmegye"
+            />
+          </a>
+        </LazyLoad>
+        <LazyLoad className="all-center" debounce={false} offsetVertical={500}>
+          <a href="https://www.migorkat.hu" target="_blank" rel='noreferrer noopener'>
+            <img
+              className="icon-links"
+              src={miskolciEgyhazmegye}
+              alt="Miskolci Egyházmegye"
+            />
+          </a>
+        </LazyLoad>
+        <LazyLoad className="all-center" debounce={false} offsetVertical={500}>
+          <a href="http://www.mariapocskegyhely.eu" target="_blank" rel='noreferrer noopener'>
+            <img
+              className="icon-links"
+              src={mariapocsNemzetiKegyhely}
+              alt="Szent Lukács Görögkatolikus Szeretet"
+            />
+          </a>
+        </LazyLoad>
+        <LazyLoad className="all-center" debounce={false} offsetVertical={500}>
+          <a href="https://www.szentlukacsszeretetszolgalat.hu" target="_blank" rel='noreferrer noopener'>
+            <img
+              className="icon-links"
+              src={szentLukacsGkatSzeretetszolgalat}
+              alt="Szent Lukács Görögkatolikus Szeretet"
+            />
+          </a>
+        </LazyLoad>
+      </div>
       <div className="bg-dark">
-        <div className='all-center'>
-          <img
-            src={ICXC_NIKA}
-            style={{
-              height: "50px",
-              width: "50px",
-              display: "inline-block",
-              marginRight: "0.7rem",
-            }}
-            alt="logo"
-          />
-        </div>
         <div>
-          <p className='small text-center'>Hajdúsámsoni Görögkatolikus Parókia &copy; 2021</p>
+          <p className="small text-center p-1">
+            Hajdúsámsoni Görögkatolikus Parókia &copy; 2021
+          </p>
         </div>
       </div>
     </div>
